ac02fe12dae2b990859861a76376abab510cdca3
Tircob Daniel <d.tircob95@gmail.com>
2019-07-10 13:25:04 +0200

    global style css

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
+import "../components/blog.css"
 
 export default class blog extends Component {
   render() {
-    return <Layout>Blog page</Layout>
+    return (
+      <Layout>
+        <h1 className="blog-title">Blog page</h1>
+      </Layout>
+    )
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,6 @@ import Layout from "../components/Layout"
 
 export default () => (
   <Layout>
-    <h1 style={{ color: "red" }}>this is our page</h1>
+    <h1>this is our page</h1>
   </Layout>
 )
