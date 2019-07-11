15aa07099b7acaa3db5a4458e7c14820fdb41ce0
Tircob Daniel <d.tircob95@gmail.com>
2019-07-11 12:53:39 +0200

    added styled components plugin / basic image component

---
+++ src/examples/Images.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import styled from "styled-components"
+
+const Images = () => {
+  return (
+    <div>
+      <h1>Images component</h1>
+    </div>
+  )
+}
+
+export default Images
