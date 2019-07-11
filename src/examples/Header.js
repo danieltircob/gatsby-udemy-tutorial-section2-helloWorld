89f28b1ca737cc29e2e929308e34c5d410d2b621
Tircob Daniel <d.tircob95@gmail.com>
2019-07-11 08:27:24 +0200

    using aliases in the query

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,29 +1,31 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-const Header = () => {
-  const {
-    site: { siteMetadata: info },
-  } = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          author
-          title
-          data {
-            age
-          }
+const getData = graphql`
+  {
+    site {
+      siteMetadata {
+        johnauthor: author
+        title
+        data {
+          age
         }
       }
     }
-  `)
+  }
+`
+
+const Header = () => {
+  const {
+    site: { siteMetadata: info },
+  } = useStaticQuery(getData)
 
   console.log()
 
   return (
     <div>
       {/* <h1>Author: {data.site.siteMetadata.author}</h1> */}
-      <h1>Author: {info.author}</h1>
+      <h1>Author: {info.johnauthor}</h1>
       <h1>Title {info.title} </h1>
     </div>
   )
