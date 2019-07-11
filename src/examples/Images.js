1d63ad311e8f7426c62fbd623a2628d63d0cf326
Tircob Daniel <d.tircob95@gmail.com>
2019-07-11 14:55:43 +0200

    added gatsby images with graphql

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -1,13 +1,66 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import styled from "styled-components"
+import img from "../images/istockphoto-1.jpg"
+import Image from "gatsby-image"
+
+const getImages = graphql`
+  {
+    fixed: file(relativePath: { eq: "istockphoto-2.jpg" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    fluid: file(relativePath: { eq: "istockphoto-3.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Images = () => {
+  const data = useStaticQuery(getImages)
+
   return (
-    <div>
-      <h1>Images component</h1>
-    </div>
+    <Wrapper>
+      <article>
+        <h3>basic Image</h3>
+        <img src={img} className="basic" alt="test img src" />
+      </article>
+      <article>
+        <h3>fixed image</h3>
+        <Image fixed={data.fixed.childImageSharp.fixed} />
+      </article>
+      <article>
+        <h3>fluid image</h3>
+        <Image fluid={data.fluid.childImageSharp.fluid} />
+      </article>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  text-align: center;
+  text-transform: capitalize;
+  width: 80vw;
+  margin: auto 0 auto 10rem;
+  .basic {
+    width: 100%;
+  }
+  article {
+    border: 3px solid red;
+    padding: 1rem 0;
+  }
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-column-gap: 1rem;
+  }
+`
 
 export default Images
