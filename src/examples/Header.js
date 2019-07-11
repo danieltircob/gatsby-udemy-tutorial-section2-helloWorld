0eb2afe9633eb2b21b1d374b042eff4a28ca8302
Tircob Daniel <d.tircob95@gmail.com>
2019-07-11 08:52:28 +0200

    added query name

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 const getData = graphql`
-  {
+  query Metadata {
     site {
       siteMetadata {
         johnauthor: author
